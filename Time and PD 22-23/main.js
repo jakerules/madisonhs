278bfab9cd57a45f6d9a4fc5c478a85f59ae326d
jakerules <jakerules@users.noreply.github.com>
2023-03-10 11:06:07 -0500

    1st

---
+++ Time and PD 22-23/main.js
@@ -0,0 +1,48 @@
+// Get the current day and time
+function updateTime() {
+  var now = new Date();
+  var day = now.getDay(); // 0 = Sunday, 1 = Monday, etc.
+  var hours = now.getHours();
+  var minutes = now.getMinutes();
+
+  // Convert the hours to 12 hour format
+  var ampm = hours >= 12 ? 'pm' : 'am';
+  hours = hours % 12;
+  hours = hours ? hours : 12; // the hour '0' should be '12'
+  var time = hours + ':' + ('0' + minutes).slice(-2) + ' ' + ampm;
+
+  // Find the current period
+  var currentPeriod;
+  var currentPeriodName;
+  var currentPeriodStart;
+  var currentPeriodEnd;
+  var periods = day === 3 ? SCHEDULE.wednesday : SCHEDULE.otherDays;
+  for (var i = 0; i < periods.length; i++) {
+    var period = periods[i];
+    if (time >= period.start && time < period.end) {
+      currentPeriod = i + 1;
+      currentPeriodName = period.name;
+      currentPeriodStart = period.start;
+      currentPeriodEnd = period.end;
+      break;
+    }
+  }
+
+  // Update the schedule widget
+  var currentPeriodEl = document.getElementById('current-period');
+  var periodNameEl = document.getElementById('period-name');
+  var timeRangeEl = document.getElementById('time-range');
+  var currentTimeEl = document.getElementById('current-time');
+  if (currentPeriod) {
+    currentPeriodEl.textContent = 'Period ' + currentPeriod;
+    timeRangeEl.textContent = currentPeriodStart + ' - ' + currentPeriodEnd;
+  } else {
+    currentPeriodEl.textContent = 'No classes currently in session';
+    periodNameEl.textContent = '';
+    timeRangeEl.textContent = '';
+  }
+  currentTimeEl.textContent = 'Current time: ' + time;
+}
+
+// Run the updateTime function every minute
+setInterval(updateTime, 30000);
---
+++ Time and PD 22-23/times.js
@@ -0,0 +1,24 @@
+var SCHEDULE = {
+  wednesday: [
+    {name: 'Period 1', start: '7:25 AM', end: '8:01 AM'},
+    {name: 'Period 2', start: '8:05 AM', end: '8:41 AM'},
+    {name: 'Period 3', start: '8:45 AM', end: '9:21 AM'},
+    {name: 'Period 4', start: '9:25 AM', end: '10:01 AM'},
+    {name: 'Period 5', start: '10:05 AM', end: '10:41 AM'},
+    {name: 'Period 6', start: '10:45 AM', end: '11:21 AM'},
+    {name: 'Period 7', start: '11:25 AM', end: '12:01 PM'},
+    {name: 'Period 8', start: '12:05 PM', end: '12:41 PM'},
+    {name: 'Period 9', start: '12:45 PM', end: '1:21 PM'}
+  ],
+  otherDays: [
+    {name: 'Period 1', start: '7:20 AM', end: '8:06 AM'},
+    {name: 'Period 2', start: '8:10 AM', end: '8:56 AM'},
+    {name: 'Period 3', start: '9:00 AM', end: '9:48 AM'},
+    {name: 'Period 4', start: '9:52 AM', end: '10:38 AM'},
+    {name: 'Period 5', start: '10:42 AM', end: '11:28 AM'},
+    {name: 'Period 6', start: '11:32 AM', end: '12:18 PM'},
+    {name: 'Period 7', start: '12:22 PM', end: '1:08 PM'},
+    {name: 'Period 8', start: '1:12 PM', end: '1:58 PM'},
+    {name: 'Period 9', start: '2:02 PM', end: '2:48 PM'}
+  ]
+};
